b1b7570bd71db75187de81ac114adcfde1970226
Leonel Hernandez <leonelfhernandez@outlook.com>
2024-08-29 19:51:37 -0300

    indice sendo calculado e dois dígitos sendo lidos

--- intel/intel1.asm
+++ intel/intel1.asm
@@ -5,8 +5,7 @@
 ; Definindo o modelo de memória (small)
 .model small
 
-; Definindo o tamanho da pilha para 256 bytes (H100)
-.stack 100h
+.stack 
 
 ; Declarando as variáveis e o buffers
 .data
@@ -22,8 +21,10 @@
     x_coord db 0
     y_coord db 0
     id_rainha db 0
+	dez db 10
 	
 	;Mensagem para exibir coordenadas e ID
+	msgok db "OK $"
 	msg_id db 'ID: $'
     msg_x db ' X: $'
     msg_y db ' Y: $'
@@ -62,25 +63,15 @@ leitura:
     mov cx, 100         ; Tamanho do buffer para 100 bytes
     int 21h             ; Interrupção do DOS
     cmp ax, 0           ; Checar se fim do arquivo (Compara o número de bytes lidos com 0)
-    je imprimir    	; Se AX for = 0, pula para fim_programa IMPRIMIR
+    ;je imprimir    	; Se AX for = 0, pula para fim_programa IMPRIMIR
 
-    ; Exibir o conteúdo lido do arquivo na tela
-    mov cx, ax          ; Número de bytes lidos
-	mov quantidadebytes, ax
-    mov si, offset buffer
-	
-loop_impressao:
-    lodsb               ; Carregar byte de [SI] em AL e incrementar SI
-    mov ah, 0Eh         ; Função para exibir caractere
-    int 10h             ; Interrupção para exibir o caractere
-    loop loop_impressao     ; Repetir para todos os caracteres lido
-    ;Continua a leitura até o fim do arquivo  
-	lea si, buffer
+	mov quantidadebytes, ax ; Número de bytes lidos
 	mov cx, quantidadebytes
+    lea si , buffer
 	
 percorrendo_buffer:
 	mov al, [si]
-	cmp al, '#'		;Verifica se é uma linha de posição inicial
+	cmp al, "#"	;Verifica se é uma linha de posição inicial
 	jz 	lendo_linha_inicial
 	inc si
 	loop percorrendo_buffer
@@ -88,123 +79,62 @@ percorrendo_buffer:
 	
 	
 lendo_linha_inicial:
-	;Lendo o ID da rainha
-	inc si
-	mov al, [si+1] ;Ler o ID da rainha, assumindo que está no índice 2
-    sub al, '0'        ;Converter de ASCII para valor numérico
+	
+	lea dx, msgok	;Carrega a mensagem de ID inválido
+	mov ah, 09h					;Função para exibir string
+	int 21h			
+	
+	add si, 2
+	mov al, [si] ;Ler o ID da rainha, assumindo que está no índice 2
+    sub al, "0" 
+	;Converter de ASCII para valor numérico
 	;Verifica se o ID da rainha é um número entre 0 e 9   (NÃO FUNCIONA PARA DOIS DÍGITOS)
-	cmp id_rainha, 0 	;Compara o valor atual com 0
+	cmp al, 0 	;Compara o valor atual com 0
 	jl erro_id			;Se for menor que 0, pula para erro ID
-	cmp id_rainha, 9	;Compara o valor atual com 9
+	cmp al, 9	;Compara o valor atual com 9
 	jg erro_id			;Se for maior do que 9, pula para erro ID
+   
     mov id_rainha, al  ;Armazenar o ID da rainha em id_rainha
-	
-	;Lendo coordenada x
-	mov al, [si+3]	;Pegando o primeiro dígito de x, assumindo que ele está no índice 2
-	sub al, '0'
-	mov x_coord, al
-	;Armazenando x_coord no vetor x_positions
-	mov bx, contador_pos
-	mov [x_positions + bx], al
-		
-	;Verificando se existe segundo dígito em x
-    ;inc si
-    ;mov ah, [si]         
-    ;cmp ah, ','          
-    ;jne ler_segundo_digito_x
-	
-	;Lendo coordenada y se não houver segundo dígito para X
-	;inc si
-	;inc si
-	;mov al, [si]
-	;sub al, '0'
-	;mov y_coord, al
-	;jmp imprimir
-	
-;ler_segundo_digito_x:
-	;sub ah, '0'         
-    ;mov al, x_coord
-    ;imul al, 10          ; Multiplicar o primeiro dígito por 10
-    ;add al, ah           ; Adicionar o segundo dígito
-    ;mov x_coord, al      ; Armazenar a coordenada X completa
-	
-	; Lendo a coordenada Y
-    mov al, [si+5]         ; Ler o primeiro dígito da coordenada Y
-    sub al, '0'          ; Converter de ASCII para valor numérico
-    mov y_coord, al      ; Armazenar o primeiro dígito em y_coord
-	
-	;Armazenando y_coord no vetor y_positions
-	mov bx, contador_pos
-	mov [y_positions + bx], al
+	mov ah, 0
+	mov di, ax
 	
 	
-	; Armazenando coordenadas nos vetores
-	;Incrementando contador para a próxima rainha
-	inc contador_pos
+	add si, 2
+	mov ax, 0
+loop_leX: ;Lendo a coordenada X
+	mul dez
+	add al, [si];Pegando o primeiro dígito de x, assumindo que ele está no índice 2
+	sub al, "0"
+	inc si
+	mov dl, [si]
+	cmp dl, ","
+	jnz loop_leX
+	lea bx, x_positions
+	mov [bx+di], al
 	
-
-	;Verificando se existe segundo dígito em y
 	
+	inc si
+	mov ax, 0
+loop_leY:		;Lendo a coordenada Y
+	mul dez
+	add al, [si]
+    sub al, "0"        ; Ler o primeiro dígito da coordenada Y
+    inc si                   ; Converter de ASCII para valor numérico
+    mov dl, [si]
+	cmp dl, 13
+	jnz loop_leY
+	lea bx, y_positions
+	mov [bx+di], al
 	
-imprimir:
-	;Exibir ID, coordenada x e coordenada y
-    lea dx, msg_id
-    mov ah, 09h
-    int 21h
-    mov al, id_rainha
-    add al, '0'        ; Converter valor numérico para ASCII
-    mov ah, 0Eh        ; Função para exibir caractere
-    int 10h            ; Interrupção para exibir o caractere
-	
-	;Exibindo coordenada x
-	lea dx, msg_x
-	mov ah, 09h
-	int 21h
-	mov al, x_coord
-	add al, '0'
-	mov ah, 0Eh
-	int 10h
-	
-	;Exibindo coordenada y
-	lea dx, msg_y
-	mov ah, 09h
-	int 21h
-	mov al, y_coord
-	add al, '0'
-	mov ah, 0Eh
-	int 10h
-	
-	;Exibindo vetor X
-	lea dx, vetorx
-	mov ah, 09h
-	int 21h
-	mov al, [x_positions + bx]
-	add al, '0'
-	mov ah, 0Eh
-	int 10h
-	
-	;Exibindo vetor Y
-	lea dx, vetory
-	mov ah, 09h
-	int 21h
-	mov al, [y_positions + bx]
-	add al, '0'
-	mov ah, 0Eh
-	int 10h
-	
-    ;Pulando para a próxima linha
-    lea dx, newline
-    mov ah, 09h
-    int 21h
 	jmp percorrendo_buffer
-
-
+;==========================================================
+	
 erro_abertura:
-    ; Exibir mensagem de erro ao abrir o arquivo
-    lea dx, msgerroabertura
-    mov ah, 9h
-    int 21h
-    jmp fim_programa
+    ;Exibir mensagem de erro ao abrir o arquivo
+   lea dx, msgerroabertura
+   mov ah, 9h
+   int 21h
+   jmp fim_programa
 
 erro_id:
 	;Exibir mensagem de erro para ID inválido
@@ -213,15 +143,18 @@ erro_id:
 	int 21h						;Interrupção para exibir o Erro
 	jmp fim_programa
 
+		
 fim_programa:
     ; Fechar o arquivo (certifique-se de que BX ainda contém o handle do arquivo)
     mov ah, 3Eh         ; Função para fechar arquivo
     int 21h             ; Interrupção DOS para fechar arquivo
-	lea bx, x_positions
-	mov dh, [bx + 1]
 	lea bx, y_positions
-	mov dl, [bx + 1]
-    ; Finalizar o programa
+	mov cl, [bx+7]	
+	lea bx, x_positions
+	mov ch, [bx+7]
+	
+	
+	;Encerra programa
     mov ah, 4Ch         ; Função para terminar programa
     int 21h             ; Interrupção DOS para encerrar programa
 
